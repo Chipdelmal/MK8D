a52073ce6bdded045db11009d9b899f722da2f5d
chipdelmal <chipdelmal@gmail.com>
2020-11-23 09:04:29 -0600

    MInor update on dictionaries

--- MK8D/constants.py
+++ MK8D/constants.py
@@ -1,6 +1,6 @@
 
 
-TRACKS = [
+TRACKS = (
     'Mario Kart Stadium', 'Water Park', 'Sweet Sweet Canyon', 'Thwomp Ruins',
     'Mario Circuit', 'Toad Harbor', 'Twisted Mansion', 'Shy Guy Falls',
     'Sunshine Airport', 'Dolphin Shoals', 'Electrodrome', 'Mount Wario',
@@ -13,33 +13,21 @@ TRACKS = [
     "Wii Wario's Gold Mine", 'SNES Rainbow Road', 'Ice Ice Outpost', 'Hyrule Circuit',
     'GCN Baby Park', 'GBA Cheese Land', 'Wild Woods', 'Animal Crossing',
     '3DS Neo Bowser City', 'GBA Ribbon Road', 'Super Bell Subway','Big Blue'
-]
+)
 
 CUPS = {
-    'Mushroom': 
-        ['Mario Kart Stadium', 'Water Park', 'Sweet Sweet Canyon', 'Thwomp Ruins'],
-    'Flower': 
-        ['Mario Circuit', 'Toad Harbor', 'Twisted Mansion', 'Shy Guy Falls'],
-    'Star':     
-        ['Sunshine Airport', 'Dolphin Shoals', 'Electrodrome', 'Mount Wario'],
-    'Special':  
-        ['Cloudtop Cruise', 'Bone-Dry Dunes', "Bowser's Castle", 'Rainbow Road'],
-    'Shell':    
-        ['Wii Moo Moo Meadows', 'GBA Mario Circuit', 'DS Cheep Cheep Beach', "N64 Toad's Turnpike"],
-    'Banana':   
-        ['GCN Dry Dry Desert', 'SNES Donut Plains 3', 'N64 Royal Raceway', '3DS DK Jungle'],
-    'Leaf':     
-        ['DS Wario Stadium', 'GCN Sherbet Land', '3DS Music Park', 'N64 Yoshi Valley'],
-    'Lightning':
-        ['DS Tick-Tock Clock', '3DS Piranha Plant Slide', 'Wii Grumble Volcano', 'N64 Rainbow Road'],
-    'Egg': 
-        ['GCN Yoshi Circuit', 'Excitebike Arena', 'Dragon Driftway', 'Mute City'],
-    'Triforce': 
-        ["Wii Wario's Gold Mine", 'SNES Rainbow Road', 'Ice Ice Outpost', 'Hyrule Circuit'],
-    'Crossing': 
-        ['GCN Baby Park', 'GBA Cheese Land', 'Wild Woods', 'Animal Crossing'],
-    'Bell':  
-        ['3DS Neo Bowser City', 'GBA Ribbon Road', 'Super Bell Subway','Big Blue']
+    'Mushroom': ['Mario Kart Stadium', 'Water Park', 'Sweet Sweet Canyon', 'Thwomp Ruins'],
+    'Flower': ['Mario Circuit', 'Toad Harbor', 'Twisted Mansion', 'Shy Guy Falls'],
+    'Star': ['Sunshine Airport', 'Dolphin Shoals', 'Electrodrome', 'Mount Wario'],
+    'Special': ['Cloudtop Cruise', 'Bone-Dry Dunes', "Bowser's Castle", 'Rainbow Road'],
+    'Shell': ['Wii Moo Moo Meadows', 'GBA Mario Circuit', 'DS Cheep Cheep Beach', "N64 Toad's Turnpike"],
+    'Banana': ['GCN Dry Dry Desert', 'SNES Donut Plains 3', 'N64 Royal Raceway', '3DS DK Jungle'],
+    'Leaf': ['DS Wario Stadium', 'GCN Sherbet Land', '3DS Music Park', 'N64 Yoshi Valley'],
+    'Lightning': ['DS Tick-Tock Clock', '3DS Piranha Plant Slide', 'Wii Grumble Volcano', 'N64 Rainbow Road'],
+    'Egg': ['GCN Yoshi Circuit', 'Excitebike Arena', 'Dragon Driftway', 'Mute City'],
+    'Triforce': ["Wii Wario's Gold Mine", 'SNES Rainbow Road', 'Ice Ice Outpost', 'Hyrule Circuit'],
+    'Crossing': ['GCN Baby Park', 'GBA Cheese Land', 'Wild Woods', 'Animal Crossing'],
+    'Bell': ['3DS Neo Bowser City', 'GBA Ribbon Road', 'Super Bell Subway','Big Blue']
 }
 
 CUPS_ORDER = (
