3755642f7dfa7396126fce865f307ffcd30a5877
chipdelmal <chipdelmal@gmail.com>
2020-12-22 17:19:00 -0600

    Updating color functions and plots export HTML

--- MK8D/color.py
+++ MK8D/color.py
@@ -1,14 +1,17 @@
 
 from colour import Color
 
-def generateColorSwatch(baseColor, levels, alphaOffset=.1, lumaOffset=.1):
+def generateColorSwatch(
+    baseColor, levels,
+    alphaOffset=(.1, .9), lumaOffset=(.1, 1),
+):
     colorsList = [None] * levels
     for i in range(levels):
         c = Color(baseColor)
-        baseLum = 1 - c.get_luminance()
-        lumLevel = (baseLum - ((1-baseLum)/levels * i))
+        baseLum = lumaOffset[1] - c.get_luminance()
+        lumLevel = (baseLum - ((lumaOffset[1]-baseLum)/levels * i))
         c.set_luminance(lumLevel)
         rgb = c.get_rgb()
-        alphaLevel = alphaOffset + ((1-alphaOffset) * i/levels)
+        alphaLevel = alphaOffset[0] + ((alphaOffset[1]-alphaOffset[0]) * i/levels)
         colorsList[i] = (rgb[0], rgb[1], rgb[2], alphaLevel)
     return colorsList
--- MK8D/dev/dev.py
+++ MK8D/dev/dev.py
@@ -2,6 +2,7 @@
   
 import time 
 from datetime import datetime
+from datetime import timedelta
 import MK8D as mk
 import numpy as np
 from os import path
@@ -12,22 +13,11 @@ import plotly.graph_objects as go
 
 
 ###############################################################################
-# Generate and export one dataframe
-###############################################################################
-(PT_FL, FILENAME, OUT) = (
-    './data/',
-    'Mario Kart 8 Deluxe - 48 Tracks (200cc, Digital, No Items).lss',
-    'MK8D.csv'
-)
-# FILEPATH = path.join(PT_FL, FILENAME)
-# data = mk.getRunsDataframeFromFile(FILEPATH, metadata=True)
-# data.to_csv(path.join(PT_FL, OUT), index=False)
-
-###############################################################################
 # Concatenate and export one dataframe from files list
 ###############################################################################
+(PT_FL, PT_PL) = ('./data/', './plots')
 FILENAMES = (
-    'Mario Kart 8 Deluxe - 48 Tracks (200cc, Cartridge, No Items).lss', 
+    # 'Mario Kart 8 Deluxe - 48 Tracks (200cc, Cartridge, No Items).lss', 
     'Mario Kart 8 Deluxe - 48 Tracks (200cc, Digital, No Items).lss', 
 )
 OUT = 'MK8D.csv'
@@ -43,16 +33,18 @@ fshdRunsIDs = sorted(list(mk.getFinishedRunsID(data, tracksFltr)))
 fshdRuns = mk.getFinishedRuns(data, tracksFltr)
 runsCTimes = mk.convertFinishedRunsToCTimes(fshdRuns, fshdRunsIDs, tracksFltr)
 
-data['ID'].unique()
-
 ###############################################################################
 # Traces plot
 ###############################################################################
+colorSwatch = mk.generateColorSwatch(
+    '#233090', len(fshdRunsIDs), alphaOffset=(.1, 1), lumaOffset=(.05, 1)
+)
 fig = px.line(
     mk.convertTimeFromSec(runsCTimes, timeTarget='Hours'),
-    x="Track", y="Time", color='ID'
+    x="Track", y="Time", color='ID',
+    color_discrete_sequence=['rgba' + str(i) for i in colorSwatch],
 )
-fig
+fig.write_html(path.join(PT_PL, 'Traces.html'))
 
 ###############################################################################
 # Center CTimes around value
@@ -63,8 +55,9 @@ runsCTimesC = mk.centerRunsCTimes(runsCTimes, centerFunction=cFun)
 ###############################################################################
 # Fastest/Slowest segments
 ###############################################################################
-(fst, slw) = (
+(fst, men, slw) = (
     {track: np.min(data[data['Track'] == track]['Time']) for track in tracksFltr},
+    {track: np.mean(data[data['Track'] == track]['Time']) for track in tracksFltr},
     {track: np.max(data[data['Track'] == track]['Time']) for track in tracksFltr}
 )
 
@@ -72,7 +65,7 @@ runsCTimesC = mk.centerRunsCTimes(runsCTimes, centerFunction=cFun)
 # Traces Plot
 ###############################################################################
 colorSwatch = mk.generateColorSwatch(
-    '#233090', len(fshdRunsIDs), alphaOffset=.1, lumaOffset=.05
+    '#233090', len(fshdRunsIDs), alphaOffset=(.1, .8), lumaOffset=(.05, 1)
 )
 runsCTimesC = mk.convertTimeFromSec(runsCTimesC, timeTarget='Minutes')
 runsCTimesC['Total'] = [time.strftime("%H:%M:%S", time.gmtime(i)) for i in runsCTimes['Time']]
@@ -81,24 +74,43 @@ fig = px.line(
     color_discrete_sequence=['rgba' + str(i) for i in colorSwatch],
     hover_data=['Total']
 )
-fig.update_traces(line=dict(width=0.5))
+fig.update_traces(line=dict(width=2))
 fig.update_xaxes(
-    range=[-1, 60], tickvals=tracksFltr, tickfont_size=8
+    range=[-1, 50], tickvals=tracksFltr, tickfont_size=8
 )
 finalCoord = runsCTimesC[runsCTimesC['Track'] == tracksFltr[-1]][cFun.__name__ + ' offset']
 finalTime = runsCTimesC[runsCTimesC['Track'] == tracksFltr[-1]]['Total']
-fig.add_trace(go.Scatter(
-    x=[1+len(tracksFltr)] * len(fshdRunsIDs),
-    y=finalCoord,
-    mode="text",
-    name="Final Times",
-    text=finalTime,
-    textfont={'size': 8},
-    textposition="middle right"
-))
+fig.add_trace(
+        go.Scatter(
+        x=[len(tracksFltr)+1] * len(fshdRunsIDs),
+        y=finalCoord,
+        mode="text", showlegend=False,
+        name="Final Times",
+        text=finalTime,
+        textfont={'size': 12},
+        textposition="middle right"
+    )
+)
+fig.write_html(path.join(PT_PL, 'TracesCentered.html'))
 
 ###############################################################################
-# Violin Plot
+# Violin Plot (Full)
+###############################################################################
+fig = go.Figure()
+fig.add_trace(
+    go.Violin(
+        x=data['Track'],
+        y=data['Time'],
+    points=False, line_color='blue', spanmode='hard',
+        line={'width': .75}
+    )
+)
+fig.update_traces(meanline_visible=True)
+fig.update_layout(violingap=0, violinmode='overlay')
+fig.write_html(path.join(PT_PL, 'ViolinsSingle.html'))
+
+###############################################################################
+# Violin Plot (Digital/Cartridge)
 ###############################################################################
 fig = go.Figure()
 fig.add_trace(
@@ -106,7 +118,8 @@ fig.add_trace(
         x=data['Track'][data['Version']=='Digital'],
         y=data['Time'][data['Version']=='Digital'],
         legendgroup='Digital', scalegroup='Digital', name='Digital',
-        side='negative', points=False, line_color='blue'
+        side='negative', points=False, line_color='blue', spanmode='hard',
+        line={'width': .75}
     )
 )
 fig.add_trace(
@@ -114,8 +127,10 @@ fig.add_trace(
         x=data['Track'][data['Version']=='Cartridge'],
         y=data['Time'][data['Version']=='Cartridge'],
         legendgroup='Cartridge', scalegroup='Cartridge', name='Cartridge',
-        side='positive', points=False, line_color='red'
+        side='positive', points=False, line_color='red', spanmode='hard',
+        line={'width': .75}
     )
 )
 fig.update_traces(meanline_visible=True)
 fig.update_layout(violingap=0, violinmode='overlay')
+fig.write_html(path.join(PT_PL, 'ViolinsDual.html'))
