0c77b4b85fd9030ac52493f3af9ee0e21d72a55d
chipdelmal <chipdelmal@gmail.com>
2021-01-02 18:26:00 -0600

    Time table is now colored (needs PB highlight)

--- MK8D/dev/MK8D_plt_runs2tbl.py
+++ MK8D/dev/MK8D_plt_runs2tbl.py
@@ -3,6 +3,7 @@
 import time 
 from datetime import datetime
 from datetime import timedelta
+import numpy as np
 import pandas as pd
 import plotly.express as px
 from colour import Color
@@ -13,13 +14,14 @@ import MK8D as mk
 
 (PT_DT, PT_PL) = ('./data/MK8D_runs.csv', './plots/Table.html')
 CATS = ('Nitro', 'Retro', 'Bonus', '32', '48')
+BCOL = 'rgb(255, 255, 255)'
 ###############################################################################
 # Read data
 ###############################################################################
 runs = pd.read_csv(PT_DT)
 # Empty dataframe to hold times -----------------------------------------------
-(dfT, dfH) = (pd.DataFrame(), pd.DataFrame())
-(dfT['ID'], dfH['ID']) = (runs['ID'].unique(), runs['ID'].unique())
+(dfT, dfH, dfR) = [pd.DataFrame() for i in range(3)]
+dfT['ID'] = dfH['ID'] = dfR['ID'] = runs['ID'].unique()
 # Go through categories populating times list ---------------------------------
 for cat in CATS:
     tracks = mk.CATEGORIES[cat]
@@ -31,16 +33,34 @@ for cat in CATS:
 for cat in CATS:
     dfH[cat] = dfT[cat].apply(lambda x: (str(timedelta(seconds=x))[:mk.TPREC]))
 ###############################################################################
-# Human-Readable
+# Table
 ###############################################################################
+colorSwatch = mk.generateColorSwatch(
+    '#233090', len(dfT['ID']), alphaOffset=(.1, 1), lumaOffset=(.35, 1)
+)
+highlight = list(Color('#EE006E').get_rgb()) + [.9]
+colors = np.array([
+    'rgb({},{},{})'.format(i[0]*255, i[1]*255, i[2]*255) for i in colorSwatch
+])
+# Rank Order ------------------------------------------------------------------
+for cat in CATS:
+    dfR[cat] = dfH[cat].rank().apply(lambda x: int(x)) 
+# Plot table ------------------------------------------------------------------
+fills = [
+    colors[dfR[cat]-1] if (cat is not 'ID') else np.array([BCOL]*len(dfT['ID'])) 
+    for cat in (['ID'] + list(CATS))
+]
 fig = go.Figure(data=[go.Table(
+    columnwidth=[100] + [200] * len(CATS),
     header=dict(
         values=list(dfH.columns),
-        align='center', font=dict(color='black', size=12)
+        align='center', font=dict(color='black', size=16),
+        line_color='black'
     ),
     cells=dict(
         values=[dfH[cat] for cat in (['ID'] + list(CATS))],
-        align='center', font=dict(color='black', size=11)
+        align='center', font=dict(color='black', size=12),
+        line_color='black', fill_color=fills
     ))
 ])
 fig.show()
