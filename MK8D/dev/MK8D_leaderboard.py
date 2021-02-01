fe49e22fd68fca7a50187d79b6971a4cd7ed0c1f
chipdelmal <chipdelmal@gmail.com>
2021-01-31 16:25:50 -0800

    Laying down the leaderboard parser

--- MK8D/dev/MK8D_leaderboard.py
+++ MK8D/dev/MK8D_leaderboard.py
@@ -1,3 +1,24 @@
 
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
-URL_BASE = 'https://www.speedrun.com/mk8dx#48_Tracks'
+URL_BASE = 'https://www.speedrun.com/mk8dx'
+URL_CAT = '48_Tracks'
+
+(DRV, OUT) = ('./driver/chromedriver', './data/')
+(TRK, SPD, ITM) = ('Nitro', '200cc', 'NoItems')
+# Load driver and mainpage ----------------------------------------------------
+print('* Loading selenium scraper...')
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+driver = webdriver.Chrome(DRV, options=chrome_options)
+driver.get(URL_BASE)
+# Select category -------------------------------------------------------------
+
+# Parse table -----------------------------------------------------------------
+table = driver.find_elements_by_tag_name('tr')
+
+i = 1
+row = table[i]
+entry = row.find_elements_by_tag_name('td')
+(rank, name, time, _, version, dateDiff, _) = [j.text for j in entry]
